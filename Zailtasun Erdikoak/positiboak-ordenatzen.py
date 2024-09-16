1e115e6d34fbf0ba353d5ab2174443e9a9404db4
NMaiztegui <naroamaiztegui@gmail.com>
2024-09-16 10:26:11 +0200

    ariketa amaitua

--- Zailtasun Erdikoak/positiboak-ordenatzen.py
+++ Zailtasun Erdikoak/positiboak-ordenatzen.py
@@ -4,10 +4,18 @@ def positiboak_ordenatu(zerrenda):
         if zenbaki > 0:
            zerrenda_ordenatua.append(zenbaki)
            zerrenda_ordenatua.sort()
-    #     else:
-    #         zerrenda_ordenatua[zenbaki] = zenbaki
-
-    print (zerrenda_ordenatua)
+   
+# iter()  convierte un iterable en un iterador. 
+    positiboak= iter(zerrenda_ordenatua)
+    emaitza =[]
+  
+# next() Función integrada que se utiliza para devolver el siguiente elemento de un iterador. 
+    for  zenbaki in zerrenda: 
+        if zenbaki > 0:
+           emaitza.append(next(positiboak))
+        else:
+            emaitza.append(zenbaki)
+    print (emaitza)
 
 
 
