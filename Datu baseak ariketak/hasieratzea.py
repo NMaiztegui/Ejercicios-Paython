b7de4523ce74da05f9dbc243d8063afc24cc34a0
NMaiztegui <naroamaiztegui@gmail.com>
2024-09-20 13:26:20 +0200

    funtzio berria

--- Datu baseak ariketak/hasieratzea.py
+++
@@ -1,9 +0,0 @@
-def taulak_sortu(kurtsorea):
-    mysql_ikasleak= "CREATE TABLE IF NOT EXISTS ikasleak( ikasle_id int NOT NULL,  izena text NOT NULL,  abizena text NOT NULL, PRIMARY KEY(ikasle_id))"
-    kurtsorea.execute(mysql_ikasleak)
-
-    mysql_ikasgaiak= "CREATE TABLE IF NOT EXISTS ikasgaiak( ikasgai_id int NOT NULL,  izena text NOT NULL,  maila text NOT NULL, hizkuntza text NOT NULL,  PRIMARY KEY(ikasgai_id))"
-    kurtsorea.execute(mysql_ikasgaiak)
-
-    mysql_notak= "CREATE TABLE IF NOT EXISTS notak( nota int NOT NULL,  oharra text NOT NULL,  maila text NOT NULL, ikasle_id int NOT NULL, ikasgai_id int NOT NULL, FOREIGN KEY (ikasgai_id) REFERENCES ikasgaiak(ikasgai_id), FOREIGN KEY (ikasle_id) REFERENCES ikasleak(ikasle_id)) "
-    kurtsorea.execute(mysql_notak)
--- Datu baseak ariketak/main.py
+++ Datu baseak ariketak/main.py
@@ -1,5 +1,6 @@
 import mysql.connector
-from hasieratzea import*
+
+from hasieratze import*
 dbConnect = {
  'host': 'localhost',
  'user': 'root',
