4ef624fc9cd0428e97e41415f221abc78d51d786
NMaiztegui <naroamaiztegui@gmail.com>
2024-10-02 13:17:50 +0200

    ikasgaik new model

--- Django/ProiektuarenIzena/Aplikazioa1/forms.py
+++ Django/ProiektuarenIzena/Aplikazioa1/forms.py
@@ -1,6 +1,11 @@
 from django import forms
-from .models import Ikasle
+from .models import Ikasle,Ikasgaiak
 class IkasleForm(forms.ModelForm):
  class Meta:
     model=Ikasle
     fields=['izena','abizena','jaiotze_data']
+
+class IkasgaiForm(forms.ModelForm):
+  class Meta:
+    model=Ikasgaiak
+    fields=['izena','maila','hizkuntza']
