7e62ac9f754f0a0705f4f9e77f963f7e5c53f4ec
NMaiztegui <naroamaiztegui@gmail.com>
2024-10-02 13:18:23 +0200

    ikasgaiak new model and form

--- Django/ProiektuarenIzena/Aplikazioa1/models.py
+++ Django/ProiektuarenIzena/Aplikazioa1/models.py
@@ -8,5 +8,14 @@ class Ikasle(models.Model):
     jaiotze_data = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f"Ikaslearen izena: {self.izena} Aabizena: {self.abizena}."
+        return f" {self.izena} {self.abizena} {self.jaiotze_data}"
 
+
+class Ikasgaiak(models.Model):
+    id=models.AutoField(primary_key=True)
+    izena=models.CharField(max_length=100)
+    maila=models.CharField(max_length=100)
+    hizkuntza=models.CharField(max_length=75)
+
+    def __str__(self) :
+        return f"{self.izena} {self.maila} {self.hizkuntza}"
