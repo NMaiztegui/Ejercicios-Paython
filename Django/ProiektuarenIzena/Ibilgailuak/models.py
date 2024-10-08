0c36a6030e779083df8e6eb89889b3924e095454
NMaiztegui <naroamaiztegui@gmail.com>
2024-10-08 18:45:33 +0200

    bezeroak, alokatuak

--- Django/ProiektuarenIzena/Ibilgailuak/models.py
+++ Django/ProiektuarenIzena/Ibilgailuak/models.py
@@ -6,6 +6,23 @@ class Kotxea(models.Model):
     marca=models.CharField(max_length=100)
     modelo=models.CharField(max_length=100)
     matricula=models.CharField(max_length=100)
+    alokatua = models.BooleanField(default=False) 
 
     def __str__(self):
         return f'{self.marca} - {self.modelo} - {self.matricula}'
+class Bezeroa(models.Model):
+    id=models.AutoField(primary_key=True)
+    izena=models.CharField(max_length=100)
+    abizena=models.CharField(max_length=100)
+    dni=models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.izena} - {self.abizena} - {self.dni}'
+
+class AlokatutakoKotxeak(models.Model):
+    kotxea=models.ForeignKey(Kotxea, on_delete=models.CASCADE)
+    bezeroa=models.ForeignKey(Bezeroa, on_delete=models.CASCADE)
+    alokatze_data=models.DateTimeField(auto_now_add=True)# se estable automaticamente la  hora y fecha en la que se ha metido a la base de datos
+
+    def __str__(self) :
+        return f'{self.kotxea} - {self.bezeroa} - Alokatua'
